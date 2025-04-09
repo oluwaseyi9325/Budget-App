59ec21ed3dcf91705fcb05b4b36b7e790e7c1e2e
oluwaseyi9325 <adedokunseyi96@gmail.com>
2025-04-09 13:07:07 +0100

    create a budget app

---
+++ src/Props.tsx
@@ -0,0 +1,10 @@
+import React, { useState } from 'react'
+
+function Props({ name,age }) {
+  
+  return (
+      <div>My name is : {name}  and my age is { age}</div>
+  )
+}
+
+export default Props
